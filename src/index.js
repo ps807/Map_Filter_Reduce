51aab291ba04302d8fdb8d15c5b2d6651137aaa3
ps807 <66031843+ps807@users.noreply.github.com>
2020-09-30 21:26:24 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+// var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+/*
+function double(x) {
+  return x * 2;
+}
+const newNumbers = numbers.map(double);
+*/
+
+/*
+var newNumbers = [];
+
+numbers.forEach(function (x) {
+  newNumbers.push(x * 2);
+});
+*/
+
+/*
+const newNumbers = numbers.map(function (x) {
+  return x * 2;
+});
+*/
+
+/*
+console.log(newNumbers);
+*/
+
+//Filter - Create a new array by keeping the items that return true.
+
+/*
+const newNumbers = numbers.filter(function (num) {
+  return num > 10;
+});
+console.log(newNumbers);
+*/
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+/*
+var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+  console.log("accumulator = " + accumulator);
+  console.log("currentNumber = " + currentNumber);
+  return accumulator + currentNumber;
+});
+console.log(newNumber);
+*/
+
+//Find - find the first item that matches from an array.
+
+/*
+const newNumber = numbers.find(function (num) {
+  return num > 10;
+});
+console.log(newNumber);
+*/
+
+//FindIndex - find the index of the first item that matches.
+
+/*
+const index = numbers.findIndex(function (num) {
+  return num > 10;
+});
+console.log(index);
+*/
+
+import emojipedia from "./emojipedia";
+
+const meanings = emojipedia.map(function (emoji) {
+  return emoji.meaning.substring(0, 100);
+});
+
+console.log(meanings);
